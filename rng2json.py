a6c8b9205df3dcb00e015a46d2823a11dc5d7efb
lifeunleaded <lifeunleaded@gmail.com>
2020-06-25 09:00:19 +0200

    update message (xml)

--- rng2json.py
+++ rng2json.py
@@ -1,123 +1,45 @@
-#!/usr/local/bin/python3
-import json,sys,pprint
-from lxml import etree
-schemafile=open(sys.argv[1],'r')
-xmlparser=etree.XMLParser(remove_comments=True,ns_clean=True)
-schema=etree.parse(schemafile,xmlparser)
-schemafile.close()
-schemaroot=schema.getroot()
-attributes={}
-elements={}
-attribute={}
-combinations={}
-element={}
-refs={}
+<?xml version="1.0" encoding="UTF-8"?>
+<article xmlns="http://docbook.org/ns/docbook" xmlns:xinfo="http://ns.expertinfo.se/cms/xmlns/1.0" xmlns:xi="http://www.w3.org/2001/XInclude" xmlns:mml="http://www.w3.org/1998/Math/MathML" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:t="http://ns.expertinfo.se/translation/xmlns/1.0" version="5.0" xinfo:resource="UUID-bf774591-c8d9-7b8a-5fe9-18de2a015964" xinfo:resource-id="1213" xinfo:resource-type="publication" xinfo:resource-title="apiguide" xinfo:resource-titlelabel="internal (POC)" xinfo:version-major="1" xinfo:version-minor="0" xml:id="UUID-bf774591-c8d9-7b8a-5fe9-18de2a015964" xml:lang="en" xinfo:time-modified="Thursday, 11-Jun-2020 06:30:58 CEST" xinfo:branched-topic-id="UUID-1f1609d8-2fd7-a1ba-e3fb-bdd9dd5b0471" xinfo:time-created="Tuesday, 09-Jun-2020 05:25:11 CEST"><info><title xinfo:text="1218"/><subtitle/><mediaobject role="titleimage"><imageobject><imagedata/></imageobject></mediaobject><volumenum xinfo:translate="no"><?placeholder Publication ID?></volumenum><pubdate xinfo:translate="no"><?placeholder Publication date?></pubdate><copyright xinfo:translate="no"><year><?placeholder Year ?></year><holder><?placeholder Holder?></holder></copyright><legalnotice xinfo:translate="no"><para><?placeholder Legal notice text?></para></legalnotice><abstract><para><?placeholder Abstract text?></para></abstract></info><?link id="1589" resource-uuid="UUID-2acc2b56-e243-dc26-668b-0908e038aea7" resource-id="1214" resource-type="fork" type="ResourceLink"?><section xml:id="UUID-2acc2b56-e243-dc26-668b-0908e038aea7" version="5.0" xml:lang="en" xinfo:resource="UUID-2acc2b56-e243-dc26-668b-0908e038aea7" xinfo:resource-id="1214" xinfo:resource-type="component fork" xinfo:resource-title="code" xinfo:resource-titlelabel="" xinfo:version-major="1" xinfo:version-minor="0" xinfo:origin="UUID-ca70e4e6-207a-c31d-8484-2bc4836de3c6" xinfo:origin-id="338" xinfo:time-modified="1590998687" xinfo:time-created="1590668483" xinfo:linktype="ResourceLink"><title xinfo:text="339"/><para xinfo:text="340"/><programlisting xml:space="preserve" xinfo:translate="no" language="python">#!/usr/bin/python
+import asyncio,os,sys,git
 
-structure={}
+print("Do stuff here")
+try:
+    sys.exit(1)
+except IOerror:
+    print("Failed")</programlisting><programlisting xml:space="preserve" xinfo:translate="no" language="json">top
+    { something
+        { something_else; 2; }
+    }
+</programlisting></section><?link id="1590" resource-uuid="UUID-23ab6db0-efa8-2652-507e-598b0c2e5739" resource-id="1215" resource-type="fork" type="ResourceLink"?><section xml:id="UUID-23ab6db0-efa8-2652-507e-598b0c2e5739" version="5.0" xml:lang="en" xinfo:resource="UUID-23ab6db0-efa8-2652-507e-598b0c2e5739" xinfo:resource-id="1215" xinfo:resource-type="component fork" xinfo:resource-title="code copy 1" xinfo:resource-titlelabel="" xinfo:version-major="1" xinfo:version-minor="0" xinfo:origin="UUID-d7a51b37-b3d9-609d-8988-4d8890a5aeb8" xinfo:origin-id="341" xinfo:time-modified="1590668912" xinfo:time-created="1590668887" xinfo:linktype="ResourceLink"><title xinfo:text="342"/><para xinfo:text="343"/><programlisting xml:space="preserve" xinfo:translate="no" language="python">#!/usr/bin/python
+import asyncio,os,sys,git
 
-unhandled={}
+print("Do stuff here")
+try:
+    sys.exit(1)
+except IOerror:
+    print("Failed")</programlisting><programlisting xml:space="preserve" xinfo:translate="no" language="json">top
+    { something
+        { something_else; 2; }
+    }
+</programlisting></section><?link id="1591" resource-uuid="UUID-ab182d55-9a84-c0f5-eb5a-ee41bcde6fb5" resource-id="1216" resource-type="fork" type="ResourceLink"?><section xml:id="UUID-ab182d55-9a84-c0f5-eb5a-ee41bcde6fb5" version="5.0" xml:lang="en" xinfo:resource="UUID-ab182d55-9a84-c0f5-eb5a-ee41bcde6fb5" xinfo:resource-id="1216" xinfo:resource-type="component fork" xinfo:resource-title="code copy 2" xinfo:resource-titlelabel="" xinfo:version-major="1" xinfo:version-minor="0" xinfo:origin="UUID-3fdbd56d-2f14-97dd-a1f6-d85fb98c2025" xinfo:origin-id="1194" xinfo:time-modified="1591005077" xinfo:time-created="1590998813" xinfo:linktype="ResourceLink"><title xinfo:text="1195"/><para xinfo:text="1196"/><programlisting xml:space="preserve" xinfo:translate="no" language="python">#!/usr/bin/python
+import asyncio,os,sys,git
 
-def populate_dict(dict,tree):
-    for attr in tree.attrib.keys():
-        if '}' in attr:
-            dict[attr.split('}')[1]]=tree.attrib[attr]
-        else:
-            dict[attr]=tree.attrib[attr]
-    if tree.text and (tree.text.strip() != ''):
-        dict['text']=tree.text
-    for subelem in tree:
-        sys.stderr.write("Subelem tag: {}\n".format(subelem.tag))
-        # if subelem.tag=='{http://relaxng.org/ns/structure/1.0}ref':
-        #     indextag=subelem.attrib['name']
-        #     dict[indextag]={'type':'ref'}
-        #     populate_dict(dict[indextag],subelem)
-        if subelem.tag in ['{http://relaxng.org/ns/structure/1.0}choice','{http://relaxng.org/ns/structure/1.0}interleave','{http://relaxng.org/ns/structure/1.0}optional']:
-            sys.stderr.write("Match\n")
-            indextag=subelem.tag.split('}')[1]
-            if indextag not in dict.keys():
-                dict[indextag]=[]
-            for subsubelem in subelem:
-                subitem={}
-                populate_dict(subitem,subsubelem)                
-                dict[indextag].append(subitem)
-        else:
-            indextag=subelem.tag.split('}')[1]
-            dict[indextag]={}
-            populate_dict(dict[indextag],subelem)
-    return dict
-
-for definition in schemaroot.findall('{http://relaxng.org/ns/structure/1.0}define'):
-    definition_content=definition[0]
-    definition_exists=(definition.attrib['name'] in refs.keys())
-    if definition_content.tag=='{http://relaxng.org/ns/structure/1.0}attribute':
-        if definition_exists:
-            sys.stderr.write("Augmenting definition {}\n".format(definition.attrib['name']))
-            attribute=attributes[definition.attrib['name']]
-            sys.stderr.write("Before: {}\n".format(attribute))
-        else:
-            attribute={}
-            try:
-                refs[definition.attrib['name']]='attribute'
-            except KeyError:
-                sys.stderr.write("Nameless attribute {}, bypassing\n".format(definition.attrib['name']))
-                continue
-        attribute=populate_dict(attribute,definition_content)
-        if definition_exists:
-            sys.stderr.write("After: {}\n".format(attribute))
-        attributes[definition.attrib['name']]=attribute
-    elif definition_content.tag=='{http://relaxng.org/ns/structure/1.0}element':
-        if definition_exists:
-            sys.stderr.write("Augmenting definition {}\n".format(definition.attrib['name']))
-            element=elements[definition.attrib['name']]
-            sys.stderr.write("Before: {}\n".format(element))
-        else:
-            element={}
-            try:
-                refs[definition.attrib['name']]='element'
-            except KeyError:
-                sys.stderr.write("Nameless element {}, bypassing\n".format(definition.attrib['name']))
-                continue
-        element=populate_dict(element,definition_content)
-        if definition_exists:
-            sys.stderr.write("After: {}\n".format(element))
-        elements[definition.attrib['name']]=element
-    else:
-        if definition_exists:
-            sys.stderr.write("Augmenting definition {}\n".format(definition.attrib['name']))
-            combination=combinations[definition.attrib['name']]
-            sys.stderr.write("Before: {}\n".format(combination))
-        else:
-            combination={}
-            try:
-                refs[definition.attrib['name']]=definition_content.tag
-            except KeyError:
-                sys.stderr.write("Nameless define child {}, bypassing\n".format(definition.attrib['name']))
-                continue
-        combination=populate_dict(combination,definition_content)
-        if definition_exists:
-            sys.stderr.write("After: {}\n".format(combination))
-        combinations[definition.attrib['name']]=combination
-
-startelem=schemaroot.find('{http://relaxng.org/ns/structure/1.0}start')
-structure['start']={}
-populate_dict(structure['start'],startelem)
-    # for choice in reference.findall('{http://relaxng.org/ns/structure/1.0}choice'):
-    #     structure['start'].append=[]    
-    # try:
-    #     refname=reference.attrib['name']
-    #     if refname in refs.keys():
-    #         print("Found reference {}, which is an {}".format(refname,refs[refname]))
-    #         if refs[refname] == 'element':
-    #             structure[refname]=elements[refname]
-    #         else:
-    #             sys.stderr.write("Non-element ref in start/choice: {}".format(refname))
-    #     else: 
-    #         sys.stderr.write("Found reference {}, from unhandled definition {}".format(refname,unhandled[refname]))
-    # except KeyError:
-    #     sys.stderr.write("Nameless reference, bypassing\n")
-
-
-
-outfile=open(sys.argv[2],'w')
-json.dump({'structure':structure,'attributes':attributes,'elements':elements,'combinations':combinations},outfile,indent=4)
-outfile.close()
+print("Do stuff here")
+try:
+    sys.exit(1)
+except IOerror:
+    print("Failed")</programlisting><programlisting xml:space="preserve" xinfo:translate="no" language="json">top
+    { something
+        { something_else; 2; }
+    }
+</programlisting><example xml:id="example-idm231820082814997"><title xinfo:text="1198"/><para xinfo:text="1199"/><example xml:id="example-idm231820087204283"><title xinfo:text="1200"/><programlisting xml:space="preserve" xinfo:translate="no" language="json">This {
+    is how;
+    we do it;
+    }
+in json;</programlisting></example><example xml:id="example-idm231820088790908"><title xinfo:text="1201"/><programlisting xml:space="preserve" xinfo:translate="no" language="python">#!/bin/python
+import this
+print("is how we do it")
+try:
+    print("in python3")
+except VersionError:
+    print("Oh dear")</programlisting></example><programlisting xml:space="preserve" xinfo:translate="no"><?placeholder Insert code snippet?></programlisting></example></section><?link id="1602" resource-uuid="UUID-81d95f53-730c-9b1b-a10a-0a90206489a1" resource-id="1225" resource-type="fork" type="ResourceLink"?><section version="5.0" xinfo:resource="UUID-81d95f53-730c-9b1b-a10a-0a90206489a1" xinfo:resource-id="1225" xinfo:resource-type="component fork" xinfo:resource-title="code copy 3" xinfo:resource-titlelabel="" xinfo:version-major="1" xinfo:version-minor="0" xml:id="UUID-81d95f53-730c-9b1b-a10a-0a90206489a1" xml:lang="en" xinfo:origin="UUID-034fdcf2-d5a3-2d99-e5ba-229cd1c13f57" xinfo:origin-id="1202" xinfo:time-modified="1591678409" xinfo:time-created="1591012589" xinfo:linktype="ResourceLink"><title xinfo:text="1203"/><para xinfo:text="1204"/><procedure xml:id="procedure-idm231820252370967"><step><para xinfo:text="1205"/><mediaobject><imageobject><imagedata fileref="UUID-0387905f-414a-8ad6-6894-d0bc72bd08a4" xinfo:image="UUID-0387905f-414a-8ad6-6894-d0bc72bd08a4"/></imageobject></mediaobject></step><step><para xinfo:text="1206"/></step><step><para xinfo:text="1207"/><warning><para xinfo:text="1208"/></warning></step></procedure></section><?link id="1603" resource-uuid="UUID-43125d22-ac66-a872-8a45-e0fa95a5f384" resource-id="1226" resource-type="fork" type="ResourceLink"?><section version="5.0" xinfo:resource="UUID-43125d22-ac66-a872-8a45-e0fa95a5f384" xinfo:resource-id="1226" xinfo:resource-type="component fork" xinfo:resource-title="Test-topic för översättning" xinfo:resource-titlelabel="" xinfo:version-major="1" xinfo:version-minor="0" xinfo:country="Sweden;USA" xml:id="UUID-43125d22-ac66-a872-8a45-e0fa95a5f384" xml:lang="en" xinfo:origin="UUID-82b11ce0-5987-63b0-d553-05eb17d662b4" xinfo:origin-id="1222" xinfo:time-modified="1591791281" xinfo:time-created="1591688500" xinfo:linktype="ResourceLink"><title xinfo:text="1223"/><para xinfo:text="1224"/></section></article>
