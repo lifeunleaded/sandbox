4c0de8c648c762c1f43d88b24ca27c42801b983f
Erik Rask <erik.rask@paligo.net>
2021-03-19 09:11:14 +0100

    One line in script

--- .github/scripts/unzip-and-move.sh
+++ .github/scripts/unzip-and-move.sh
@@ -1,20 +1,21 @@
-files=$(git diff-tree -r $GITHUB_SHA --name-only --no-commit-id)
-echo "Commit:"
-echo $GITHUB_SHA
-echo "Files changed:"
-echo "$files"
-for file in $files; do
-    echo "In for loop"
-    echo $file
-    if [ "${file##*.}" = "zip" ]; then 
-        echo "Found zip file"
-        unzip -d tmpoutdir $file
-        mkdir outdir
-        mv tmpoutdir/*/out/* outdir
-        rm -rf tmpoutdir
-        echo "Files in zip out/:"
-        ls outdir
-    else
-        echo "No zip file here"
-    fi
-done
+for file in $(git diff-tree -r HEAD --name-only --no-commit-id); do echo $file; done
+# files=$(git diff-tree -r $GITHUB_SHA --name-only --no-commit-id)
+# echo "Commit:"
+# echo $GITHUB_SHA
+# echo "Files changed:"
+# echo "$files"
+# for file in $files; do
+#     echo "In for loop"
+#     echo $file
+#     if [ "${file##*.}" = "zip" ]; then 
+#         echo "Found zip file"
+#         unzip -d tmpoutdir $file
+#         mkdir outdir
+#         mv tmpoutdir/*/out/* outdir
+#         rm -rf tmpoutdir
+#         echo "Files in zip out/:"
+#         ls outdir
+#     else
+#         echo "No zip file here"
+#     fi
+# done
