68e1d074007535aac0f9450fff3be2dae4b8fa96
Erik Rask <erik.rask@paligo.net>
2021-03-19 10:43:00 +0100

    Use git show HEAD

--- .github/scripts/unzip-and-move.sh
+++ .github/scripts/unzip-and-move.sh
@@ -1,24 +1,20 @@
-echo "Apa" > $GITHUB_WORKSPACE/tmpfile
-echo $GITHUB_SHA >> $GITHUB_WORKSPACE/tmpfile
-git diff-tree -r $GITHUB_SHA --name-only --no-commit-id >> $GITHUB_WORKSPACE/tmpfile
-cat $GITHUB_WORKSPACE/tmpfile
-# files=$(git diff-tree -r $GITHUB_SHA --name-only --no-commit-id)
-# echo "Commit:"
-# echo $GITHUB_SHA
-# echo "Files changed:"
-# echo "$files"
-# for file in $files; do
-#     echo "In for loop"
-#     echo $file
-#     if [ "${file##*.}" = "zip" ]; then 
-#         echo "Found zip file"
-#         unzip -d tmpoutdir $file
-#         mkdir outdir
-#         mv tmpoutdir/*/out/* outdir
-#         rm -rf tmpoutdir
-#         echo "Files in zip out/:"
-#         ls outdir
-#     else
-#         echo "No zip file here"
-#     fi
-# done
+files=$(git show $GITHUB_SHA --name-only --oneline | awk FNR-1)
+echo "Commit:"
+echo $GITHUB_SHA
+echo "Files changed:"
+echo "$files"
+for file in $files; do
+    echo "In for loop"
+    echo $file
+    if [ "${file##*.}" = "zip" ]; then 
+        echo "Found zip file"
+        unzip -d tmpoutdir $file
+        mkdir outdir
+        mv tmpoutdir/*/out/* outdir
+        rm -rf tmpoutdir
+        echo "Files in zip out/:"
+        ls outdir
+    else
+        echo "No zip file here"
+    fi
+done
