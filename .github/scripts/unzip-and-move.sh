48420df1554c621cee48b66026783f4536021b83
Erik Rask <erik.rask@paligo.net>
2021-09-01 13:41:35 +0200

    Comment code

--- .github/scripts/unzip-and-move.sh
+++ .github/scripts/unzip-and-move.sh
@@ -1,13 +1,19 @@
 files=$(git show HEAD --name-only --oneline | awk FNR-1)
+# Get the list of changed files, and output some diagnostics in the job log.
 echo "Commit:"
 echo $GITHUB_SHA
 echo "Files changed:"
 echo "$files"
 for file in $files; do
-    echo "In for loop"
+    echo "File changed:"
     echo $file
+    # Check if the list of changed files contains a zip archive.
+    # Also check that the file exists, since zip files in the list
+    # if the commit removes them.
     if [ "${file##*.}" = "zip" ] && [ -f $file ]; then 
         echo "Found zip file"
+        # Unzip and move the publication to docs/,
+        # which is the base for this Pages setup.
         unzip -d tmpoutdir $file
         cp -rf tmpoutdir/*/out/* docs/
         rm -rf tmpoutdir
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # sandbox
-Editing the readme in master
-Editing again
+This is a sample repository branch to demonstrate receiving publications from the Paligo GitHub integration and unpacking them as GitHub Pages.
